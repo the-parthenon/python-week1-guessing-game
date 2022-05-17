5426a9799d8e15dd12f83ed6011fb654b4d82702
the-parthenon <ericinportland@gmail.com>
2022-05-17 14:34:35 -0700

    Added error messages for bad guesses.

--- game.py
+++ game.py
@@ -8,19 +8,27 @@ target_number = randint(1, 100)
 
 print(player_name + ", I'm thinking of a number between 1 and 100.\nTry to guess the number.")
 
-# print("Target number: " + str(target_number))
+print("Target number: " + str(target_number))
 
 player_guess = 0
 guess_count = 1
 
 while target_number != player_guess:
-    player_guess = int(input("What's your guess\n"))
-    if player_guess > target_number:
-        guess_count += 1
-        print("Too high!  Try again.\n")
-    elif player_guess < target_number:
-        guess_count += 1
-        print("Too low!  Take another stab at it.\n")
+    player_guess = input("What's your guess\n")
+    if not player_guess.isnumeric():
+        print("Dogg, that is not even a number.  Get serious.  Penalty of 3 guesses.")
+        guess_count += 3
+    else:
+        player_guess = int(player_guess)    
+        if player_guess < 1 or player_guess > 100:
+            print("Come on, now, you know that's not in the acceptable range.\nThat's gonna cost you a guess.\n")
+            guess_count += 1
+        elif player_guess > target_number:
+            guess_count += 1
+            print("Too high!  Try again.\n")
+        elif player_guess < target_number:
+            guess_count += 1
+            print("Too low!  Take another stab at it.\n")
 
 
 print("You got it! The number was " + str(target_number) + " and you got it in " + str(guess_count) + " guesses!")
