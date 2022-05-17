29d5c43dcf6904919d0cf23f1606afeecd091693
the-parthenon <ericinportland@gmail.com>
2022-05-17 13:52:47 -0700

    Implemented player input, computer guess & initial loop logic

--- game.py
+++ game.py
@@ -1,5 +1,20 @@
 """A number-guessing game."""
 
-player_name = input("Welcome!  What can I call you?")
+from random import randint
 
-print(player_name)
+player_name = input("Welcome!  What can I call you?\n")
+
+target_number = randint(1, 100)
+
+print(player_name + ", I'm thinking of a number between 1 and 100.\nTry to guess the number.")
+
+print("Target number: " + str(target_number))
+
+player_guess = 0
+guess_count = 1
+
+while target_number != player_guess:
+    player_guess = int(input("What's your guess\n"))
+
+
+print("You got it! The number was " + str(target_number) + "and you got it in " + str(guess_count) + " guesses!")
