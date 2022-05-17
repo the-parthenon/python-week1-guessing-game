4001cc9fe05506b27d9759c15e03510af074d70b
the-parthenon <ericinportland@gmail.com>
2022-05-17 14:00:10 -0700

    Implemented guessing logic and congratulations message.

--- game.py
+++ game.py
@@ -8,13 +8,19 @@ target_number = randint(1, 100)
 
 print(player_name + ", I'm thinking of a number between 1 and 100.\nTry to guess the number.")
 
-print("Target number: " + str(target_number))
+# print("Target number: " + str(target_number))
 
 player_guess = 0
 guess_count = 1
 
 while target_number != player_guess:
     player_guess = int(input("What's your guess\n"))
+    if player_guess > target_number:
+        guess_count += 1
+        print("Too high!  Try again.\n")
+    elif player_guess < target_number:
+        guess_count += 1
+        print("Too low!  Take another stab at it.\n")
 
 
-print("You got it! The number was " + str(target_number) + "and you got it in " + str(guess_count) + " guesses!")
+print("You got it! The number was " + str(target_number) + " and you got it in " + str(guess_count) + " guesses!")
